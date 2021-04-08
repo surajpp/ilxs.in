963a0495bc0a43d676c8e4f253640e1f157d0ef9
Suraj Patil <35756836+surajpp@users.noreply.github.com>
2021-04-08 23:28:44 +0530

    DS1

--- coll.js
+++ coll.js
@@ -2,6 +2,9 @@ const navSlide = () => {
 	const collab = document.querySelector('.collab');
 	const nav = document.querySelector('.nav-links');
 	const navLinks = document.querySelectorAll('.nav-links li');
+	const dcollab = document.querySelector('.dcollab');
+	const dnav = document.querySelector('.dnav-links');
+	const dnavLinks = document.querySelectorAll('.dnav-links li');
 	
 	collab.addEventListener('click', () => {
 		nav.classList.toggle('nav-active');
@@ -15,6 +18,21 @@ const navSlide = () => {
 		});
 		collab.classList.toggle('toggle');
 	});
+
+	dcollab.addEventListener('click', () => {
+		dnav.classList.toggle('nav-active');
+		
+		dnavLinks.forEach((link, index) => {
+			if (link.style.animation) {
+				link.style.animation = '';
+		} else {
+		link.style.animation = 'dnavLinkFade 0.5s ease forwards';
+		}
+		});
+		collab.classList.toggle('toggle');
+	});
 }
 
 navSlide();
+
+		
